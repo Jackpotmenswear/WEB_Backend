d5ed1c4d0a197b9bebc8913c2f4e316895f15bb6
Jackpotmenswear <jackpotmenswear2001@gmail.com>
2024-10-12 19:09:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ store.on('error', (error) => {
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:'http://localhost:5173',credentials:true})); 
+app.use(cors({origin:'https://jackpotmens.vercel.app',credentials:true})); 
 
 app.use(session({
     secret:"hai ",
